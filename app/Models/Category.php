73180a93bd4971eee636c06330ea65c23779849d
Muhammad Ilham Purnama <ilhampurnama78@gmail.com>
2025-01-10 16:00:11 +0700

    - Setup Project - Atur schema table database - Atur filable dan ORM model - Custome method pada model - install dan bikin resource filament - CRUD category, pricing, user - install spatie(laravel permission) dan CRUD role - Atur hak akses dashboard admin

---
+++ app/Models/Category.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+
+class Category extends Model
+{
+    //
+
+    use SoftDeletes;
+
+    protected $fillable = ['name', 'slug'];
+
+    public function setNameAttribute($value) {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function courses() : HasMany
+    {
+        return $this->hasMany(Course::class);
+    }
+}
