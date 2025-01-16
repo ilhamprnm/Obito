c8116a8969a5a798738a1131aa9a8eb85729a524
Muhammad Ilham Purnama <ilhampurnama78@gmail.com>
2025-01-16 18:55:00 +0700

    [UPDATE] Implementasi master layout ke halaman, login, register, index, dan pricing

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,25 +1,6 @@
-<!doctype html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="{{ asset('output.css') }}" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
-        <title>Sign Up - Obito Online Learning Platform</title>
-        <meta name="description" content="Obito is an innovative online learning platform that empowers students and professionals with high-quality, accessible courses.">
-
-        <!-- Favicon -->
-        <link rel="icon" type="image/png" sizes="32x32" href="assets/images/logos/logo-64.png') }}">
-        <link rel="apple-touch-icon" href="assets/images/logos/logo-64.png') }}">
-
-        <!-- Open Graph Meta Tags -->
-        <meta property="og:title" content="Obito Online Learning Platform - Learn Anytime, Anywhere">
-        <meta property="og:description" content="Obito is an innovative online learning platform that empowers students and professionals with high-quality, accessible courses.">
-        <meta property="og:image" content="https://obito-platform.netlify.app/assets/images/logos/logo-64-big.png') }}">
-        <meta property="og:url" content="https://obito-platform.netlify.app">
-        <meta property="og:type" content="website">
-    </head>
-    <body>
+@extends('front.layouts.app')
+@section('title', 'Register - Obito')
+@section('content')
         <x-nav-guest/>
         <main class="relative flex flex-1 h-full">
             <section class="flex flex-1 items-center py-5 px-5 pl-[calc(((100%-1280px)/2)+75px)]">
@@ -90,5 +71,4 @@
         </main>
 
         <script src="{{ asset('js/photo-upload.js') }}"></script>
-    </body>
-</html>
+@endsection
--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -1,25 +1,6 @@
-<!doctype html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="{{ asset('output.css') }}" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
-        <title>Obito Online Learning Platform - Learn Anytime, Anywhere</title>
-        <meta name="description" content="Obito is an innovative online learning platform that empowers students and professionals with high-quality, accessible courses.">
-
-        <!-- Favicon -->
-        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/logos/logo-64.png') }}">
-        <link rel="apple-touch-icon" href="{{ asset('assets/images/logos/logo-64.png') }}">
-
-        <!-- Open Graph Meta Tags -->
-        <meta property="og:title" content="Obito Online Learning Platform - Learn Anytime, Anywhere">
-        <meta property="og:description" content="Obito is an innovative online learning platform that empowers students and professionals with high-quality, accessible courses.">
-        <meta property="og:image" content="https://obito-platform.netlify.app/assets/images/logos/logo-64-big.png') }}">
-        <meta property="og:url" content="https://obito-platform.netlify.app">
-        <meta property="og:type" content="website">
-    </head>
-    <body>
+@extends('front.layouts.app')
+@section('title', 'Obito')
+@section('content')
         <x-nav-guest/>
         <main class="flex flex-1 items-center py-[70px]">
             <div class="w-full flex gap-[77px] justify-between items-center pl-[calc(((100%-1280px)/2)+75px)]">
@@ -65,5 +46,4 @@
                 <img src="{{ asset('assets/images/backgrounds/hero-image.png') }}" alt="hero-image">
             </div>
         </main>
-    </body>
-</html>
+@endsection
